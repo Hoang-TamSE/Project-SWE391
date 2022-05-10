6cc0f475f2d9138dda627a80d36ced7f4cb0b36b
Tam <kakasika15@gmail.com>
2022-05-10 21:42:12 +0700

    demo

---
+++ src/java/sample/utils/ContextListener.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.utils;
+
+import java.io.File;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Hoang Tam
+ */
+@WebListener("application context listener")
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext context = event.getServletContext();
+        String log4jConfigFile = context.getInitParameter("log4j-config-location");
+        String fullPath = context.getRealPath("/")+File.separator+log4jConfigFile;
+        System.out.println(fullPath);
+        PropertyConfigurator.configure(fullPath);
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
